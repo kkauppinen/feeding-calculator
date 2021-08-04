175d2e64c36ad98d4ac7db7c088bd673ae9334e4
Kirsi Kauppinen <kirsu@Kirsi-MacBook-Air.local>
2021-08-04 16:36:54 +0300

    JS file created

---
+++ dogCalculator.js
@@ -0,0 +1,4 @@
+window.onload = function() {
+    document.getElementById("energy_need").addEventListener("click", calcCalorieRequirement);
+    document.getElementById("calc_feeding").addEventListener("click", calcFeedingAmount);
+}
